b37936059cdeace7a9fd401c16670ccb9203f8e5
elainapolson <elainapolson@gmail.com>
2015-07-21 18:10:17 -0400

    done

--- js/spotify-chart.js
+++ js/spotify-chart.js
@@ -13,24 +13,49 @@ $(function() {
 });
 
 function extractTop20Tracks(tracks) {
-  // your code here
+  return tracks.slice(0,20);
 }
 
 function extractNumberOfStreams(tracks) {
-  // your code here
+  var streamArray = [];
+  for (var i=0; i < tracks.length; i++) {
+    streamArray.push(tracks[i]['num_streams']);
+  }
+  return streamArray;
 }
 
 function extractNames(tracks) {
-  // your code here
+  var songArray = [];
+  var tracks = tracks;
+  for (var i=0; i < tracks.length; i++) {
+    songArray.push(tracks[i]['track_name']);
+  }
+  return songArray;
 }
 
 function chartData(labels, inputData) {
-  // your code here
-
-  // use the dataSetProperties variable defined above if it helps
+  var dataSetProperties = {
+  label: 'Spotify Chart of Top 20 Streamed Songs on Spotify with their Steam Count', 
+  fillColor: 'rgba(220,220,220,0.5)', 
+  strokeColor: 'rgba(220,220,220,0.8)', 
+  highlightFill: 'rgba(220,220,220,0.75)', 
+  highlightStroke: 'rgba(220,220,220,1)',
+  data: inputData
+  }
+  return {
+    labels: labels,
+    datasets: [dataSetProperties]
+  };
 }
 
 function getSpotifyTracks(callback){
+  var url = "http://charts.spotify.com/api/tracks/most_streamed/us/weekly/latest";
+  $.ajax ({
+    url: url,
+    contentType: 'application/json',
+    dataType: 'jsonp',
+    success: callback
+  });
   // your ajax call here, on success it should call on the 
   // parameter it's passed (it's a function), and pass it's 
   // parameter the data it received
@@ -39,6 +64,13 @@ function getSpotifyTracks(callback){
 }
 
 function success(parsedJSON) {
+  var twentyTracks = extractTop20Tracks(parsedJSON.tracks)
+  var names = extractNames(twentyTracks)
+  var streams = extractNumberOfStreams(twentyTracks)
+  var data = chartData(names, streams)
+
+  var ctx = $("#spotify-chart").get(0).getContext("2d");
+  var myBarChart = new Chart(ctx).Bar(data);
   // this function will make a new bar chart, refer to this url:
   // http://www.chartjs.org/docs/#bar-chart
   // you will need to call on:
