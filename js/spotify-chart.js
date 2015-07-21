f728aab63104ba502867027713571a80be2aef3e
elainapolson <elainapolson@gmail.com>
2015-07-21 18:14:37 -0400

    slightly refactored

--- js/spotify-chart.js
+++ js/spotify-chart.js
@@ -26,7 +26,6 @@ function extractNumberOfStreams(tracks) {
 
 function extractNames(tracks) {
   var songArray = [];
-  var tracks = tracks;
   for (var i=0; i < tracks.length; i++) {
     songArray.push(tracks[i]['track_name']);
   }
@@ -34,14 +33,7 @@ function extractNames(tracks) {
 }
 
 function chartData(labels, inputData) {
-  var dataSetProperties = {
-  label: 'Spotify Chart of Top 20 Streamed Songs on Spotify with their Steam Count', 
-  fillColor: 'rgba(220,220,220,0.5)', 
-  strokeColor: 'rgba(220,220,220,0.8)', 
-  highlightFill: 'rgba(220,220,220,0.75)', 
-  highlightStroke: 'rgba(220,220,220,1)',
-  data: inputData
-  }
+  dataSetProperties['data'] = inputData
   return {
     labels: labels,
     datasets: [dataSetProperties]
@@ -49,18 +41,12 @@ function chartData(labels, inputData) {
 }
 
 function getSpotifyTracks(callback){
-  var url = "http://charts.spotify.com/api/tracks/most_streamed/us/weekly/latest";
   $.ajax ({
     url: url,
     contentType: 'application/json',
     dataType: 'jsonp',
     success: callback
   });
-  // your ajax call here, on success it should call on the 
-  // parameter it's passed (it's a function), and pass it's 
-  // parameter the data it received
-
-  // use the url variable defined above if it helps
 }
 
 function success(parsedJSON) {
@@ -71,14 +57,4 @@ function success(parsedJSON) {
 
   var ctx = $("#spotify-chart").get(0).getContext("2d");
   var myBarChart = new Chart(ctx).Bar(data);
-  // this function will make a new bar chart, refer to this url:
-  // http://www.chartjs.org/docs/#bar-chart
-  // you will need to call on:
-  //  1. extractTop20Tracks - pass it tracks
-  //  2. extractNames -  pass it the result of #1
-  //  3. extractNumberOfStreams - pass it the result of #1
-  //  4. chartData - pass it results of #2 and #3
-  //  5. make a variable `ctx` and select the canvas with the id of spotify-chart
-  //     * also make sure to specify 2d context
-  //  6. make a new bar chart!
 }
